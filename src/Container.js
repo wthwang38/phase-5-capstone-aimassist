007992d81538519687bf9ac1a3a39a674457db37
William Hwang <willhwang90@gmail.com>
2022-07-22 10:33:24 -0400

    colors added #10

--- src/Container.js
+++ src/Container.js
@@ -1,20 +1,22 @@
 import React, {useState} from 'react'
-//import useMightyMouse from 'react-hook-mighty-mouse'
+
 
 const Container = () => {
    const [xNum, setXNum] = useState(100)
    const [yNum, setYNum] = useState(200)
-    // const { position: { client }, } = useMightyMouse();
-    // const clickPosition = () => {
-    //     console.log("x:", client.x)
-    //     console.log("y:", client.y) 
-    // }
+   //const [targetClicked, setTargetClicked] = useState(false)
+    const [newColor, setNewColor] = useState('#FF0000')
+
     const colors = ['#ee82ee', '#00FFFF', '#6495ED', '#8A2BE2', '#FFD700', '#191970', '#7fff00', '#8b00b8', '#ff1493', '#48d1cc']
+
    function getRandomNumber(min, max){
     return Math.floor(Math.random() * (max - min) + min)
    }
     function getRandomColor(){
         return colors[Math.floor(Math.random() * colors.length + 1)]
+    }
+    const color = {
+        backgroundColor: `${newColor}`
     }
     const y = {
         marginTop: `${yNum}px`
@@ -22,18 +24,31 @@ const Container = () => {
     const x = {
         marginInline: `${xNum}px`
     }
-    function handleClick(){
+
+    function createRandomCircles(){
         let x1 = getRandomNumber(1, 800)
         let y1 = getRandomNumber(1, 500)
+        let nc = getRandomColor()
+        setNewColor(nc)
         setXNum(x1)
         setYNum(y1)
+        //setTargetClicked(!targetClicked)
+        //console.log(targetClicked)
     }
-    //function createRandomCircles(){}
-    const circle = <div className="circle" style={Object.assign({}, x, y)} onClick={()=> handleClick()}></div>
+    function wasClicked(){
+        
+        console.log("miss clicked")
+    }
+    function handleClickInCircle() {
+        createRandomCircles()
+    }    
+
+   
+    const circle = <div className="circle" style={Object.assign({}, x, y, color)} onClick={()=> handleClickInCircle()}></div>
 
     return (
         // <div id="playbox" onClick={() => clickPosition()}>
-         <div id='playbox'> 
+         <div id='playbox' onClick={()=> wasClicked()}> 
             <div>{circle}</div>
         </div>
     );
