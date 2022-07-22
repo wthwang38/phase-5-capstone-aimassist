60ec70cb349986f154c4f237419caa54252153b8
William Hwang <willhwang90@gmail.com>
2022-07-22 11:44:41 -0400

    added missed counter resolved issue #8

--- src/Container.js
+++ src/Container.js
@@ -1,11 +1,16 @@
+import { click } from '@testing-library/user-event/dist/click';
 import React, {useState} from 'react'
 
 
 const Container = () => {
-   const [xNum, setXNum] = useState(100)
-   const [yNum, setYNum] = useState(200)
+   const [xNum, setXNum] = useState(1)
+   const [yNum, setYNum] = useState(1)
    //const [targetClicked, setTargetClicked] = useState(false)
     const [newColor, setNewColor] = useState('#FF0000')
+    //logs total clicks
+   const [totalCount, setTotalCount] = useState(1)
+   const [circleCount, setCircleCount] = useState(1)
+   const [timer, setTimer] = useState(0) 
 
     const colors = ['#ee82ee', '#00FFFF', '#6495ED', '#8A2BE2', '#FFD700', '#191970', '#7fff00', '#8b00b8', '#ff1493', '#48d1cc']
 
@@ -33,23 +38,32 @@ const Container = () => {
         setXNum(x1)
         setYNum(y1)
         //setTargetClicked(!targetClicked)
-        //console.log(targetClicked)
-    }
-    function wasClicked(){
         
-        console.log("miss clicked")
+    }
+
+    function clickCounter(state, setter, string){
+        let clicks = state
+        clicks+=1
+        setter(clicks)
+        console.log(string, state)
     }
     function handleClickInCircle() {
         createRandomCircles()
+        clickCounter(circleCount,setCircleCount,"circle click:")
     }    
 
-   
+   function missedClicks(){
+    const missed = totalCount - circleCount
+    return missed
+   }
     const circle = <div className="circle" style={Object.assign({}, x, y, color)} onClick={()=> handleClickInCircle()}></div>
 
     return (
-        // <div id="playbox" onClick={() => clickPosition()}>
-         <div id='playbox' onClick={()=> wasClicked()}> 
+        <div>
+            <div id="missed">Missed: {missedClicks()}</div>
+         <div id='playbox' onClick={()=> clickCounter(totalCount, setTotalCount,"total click:")}> 
             <div>{circle}</div>
+        </div>
         </div>
     );
 
