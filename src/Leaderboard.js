9d85e6f7cc71702f38fe078757da7650d396dfc4
William Hwang <willhwang90@gmail.com>
2023-01-09 16:25:52 -0500

    removed commented out codes

--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import PlayerCard from "./PlayerCard";
-//import NavBar from "./NavBar";
+
 
 
 const Leaderboard = ({updateP})=> {
--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Container from './Container'
-//import NavBar from './NavBar'
+
 const Main = ({user, updateP, setUpdateP, setUPAvg, setUPHitAcc, setUPHits, setUPMissed, setUPTotal}) => {
 
 
@@ -8,7 +8,7 @@ const Main = ({user, updateP, setUpdateP, setUPAvg, setUPHitAcc, setUPHits, setU
 
 
     <div id='main'>
-      {/* {user ? <NavBar onLogout={handleLogout} /> : null} */}
+     
       <h2>Hi, {user.username}!</h2>
       <img src={user.img_profile} style={{width: "100px"}}alt="profile"/>
       <Container user={user} updateP={updateP} setUpdateP={setUpdateP} setUPHits={setUPHits} setUPTotal={setUPTotal} setUPMissed={setUPMissed} setUPAvg={setUPAvg} setUPHitAcc={setUPHitAcc} />
